d343b3ecaac051b8d16494d5f002c835ba5a5d1d
SerhiiImp <6wserhiiw6@gmail.com>
2025-09-27 09:06:39 +0300

    Intial / reset repo

---
+++ src/main/java/com/refractorx95/Dock_demo/DockDemoApplication.java
@@ -0,0 +1,13 @@
+package com.refractorx95.Dock_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/refractorx95/Dock_demo/DockDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refractorx95.Dock_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
