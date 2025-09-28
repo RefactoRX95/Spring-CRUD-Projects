deb504b93a520a9495145ab2860c1920357a209d
SerhiiImp <6wserhiiw6@gmail.com>
2025-09-28 09:30:50 +0300

    Add Users

--- src/main/java/com/refractorx95/Book_demo/config/SecurityConfig.java
+++ src/main/java/com/refractorx95/Book_demo/config/SecurityConfig.java
@@ -2,8 +2,17 @@ package com.refractorx95.Book_demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.swing.plaf.PanelUI;
 
 @Configuration
 @EnableWebSecurity
@@ -11,6 +20,28 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService(){
+        UserDetails userDetailsOne = User.withUsername("User1")
+                .password(passwordEncoder().encode("Pass1")).build();
+        UserDetails userDetailsTwo = User.withUsername("User2")
+                .password(passwordEncoder().encode("Pass2")).build();
+        UserDetails admin = User.withUsername("Admin")
+                .password(passwordEncoder().encode("Admin1")).build();
 
+        return new InMemoryUserDetailsManager(userDetailsOne,
+                userDetailsTwo,admin);
     }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception
+    {
+        httpSecurity.authorizeHttpRequests(request ->
+                request.requestMatchers("/book-store/welcome").permitAll()
+                        .anyRequest().authenticated());
+    }
+
 }
