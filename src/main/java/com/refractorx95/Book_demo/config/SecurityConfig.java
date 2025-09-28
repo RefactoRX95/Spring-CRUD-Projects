6a74b5f14ee69ce8ccf2f1d48c0365a261d2af5e
SerhiiImp <6wserhiiw6@gmail.com>
2025-09-28 19:56:38 +0300

    Final created UserInfoUserDetail

--- src/main/java/com/refractorx95/Book_demo/config/SecurityConfig.java
+++ src/main/java/com/refractorx95/Book_demo/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.refractorx95.Book_demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -38,6 +41,9 @@ public class SecurityConfig {
                 userDetailsTwo,admin);
     }*/
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -55,6 +61,16 @@ public class SecurityConfig {
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return httpSecurity.build();
 
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider()
+    {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return daoAuthenticationProvider;
     }
 
 }
