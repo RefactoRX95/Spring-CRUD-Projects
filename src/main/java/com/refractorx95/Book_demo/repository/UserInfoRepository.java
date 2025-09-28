f14413ee15852950d2861bad38e45bf518c84ebc
SerhiiImp <6wserhiiw6@gmail.com>
2025-09-28 11:10:54 +0300

    Create UserInfoService

--- src/main/java/com/refractorx95/Book_demo/repository/UserInfoRepository.java
+++ src/main/java/com/refractorx95/Book_demo/repository/UserInfoRepository.java
@@ -3,7 +3,10 @@ package com.refractorx95.Book_demo.repository;
 import com.refractorx95.Book_demo.entity.UserInfo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserInfoRepository extends MongoRepository<UserInfo,String> {
 
+    Optional<UserInfo> findByUserName(String userName);
 
 }
---
+++ src/main/java/com/refractorx95/Book_demo/service/UserInfoService.java
@@ -0,0 +1,4 @@
+package com.refractorx95.Book_demo.service;
+
+public interface UserInfoService {
+}
